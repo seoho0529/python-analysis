40708bce21c27ce373ccbbe216a87951e00c21b3
seoho <seohotwo@naver.com>
2023-11-12 18:34:46 +0900

    analysis study

--- analysis_test/linear/linear.py
+++ analysis_test/linear/linear.py
@@ -111,6 +111,7 @@ print(crime_analysis.corr())
 # 스트레스 인지율 -0.851870  0.109529  1.000000  0.889252  0.865526
 # 인구수      -0.949905  0.076489  0.889252  1.000000  0.903435
 # 건        -0.890489 -0.161952  0.865526  0.903435  1.000000
+# 실업률은 발생건수와 상관관계가 거의 없다고 알 수 있다.
 
 # 히트맵
 import seaborn as sns
@@ -122,9 +123,36 @@ print(stats.pearsonr(crime_analysis['발생건수'], crime_analysis['CPI'])) # P
 print(stats.pearsonr(crime_analysis['발생건수'], crime_analysis['실업률'])) # PearsonRResult(statistic=-0.16195175313674026, pvalue=0.5641760129646295)
 print(stats.pearsonr(crime_analysis['발생건수'], crime_analysis['스트레스 인지율'])) # PearsonRResult(statistic=0.8655261708093139, pvalue=3.065014812312715e-05)
 print(stats.pearsonr(crime_analysis['발생건수'], crime_analysis['인구수']))  # PearsonRResult(statistic=0.903434689304182, pvalue=3.915084633601483e-06)
-# 이때, 실업률 변수를 제외한 나머지 3개의 변수는 모두 0.05보다 작으므로 종속변수인 범죄건수에 유의한 영향을 미칠 것으로 간주할 수 있다.
+# 이때, '실업률' 변수를 제외한 나머지 3개의 변수는 모두 0.05보다 작으므로 종속변수인 범죄건수에 유의한 영향을 미칠 것으로 간주할 수 있다.
+
+# bartlett을 사용한 등분산 검정
+print(stats.bartlett(crime_analysis['발생건수'], crime_analysis['CPI']))
+print(stats.bartlett(crime_analysis['발생건수'], crime_analysis['실업률']))
+print(stats.bartlett(crime_analysis['발생건수'], crime_analysis['스트레스 인지율']))
+print(stats.bartlett(crime_analysis['발생건수'], crime_analysis['인구수']))
+# 4개 변수 모두 pvalue가 0.05보다 작으므로 각 변수들이 통계적으로 유의하다.는 것을 알 수 있다.
+
+# 독립성 검정 shapiro 사용하기
+from scipy.stats import shapiro
+shapiro_results = crime_analysis.apply(lambda x: shapiro(x))
+print(shapiro_results)
+#       CPI      실업률   스트레스 인지율  인구수     발생건수
+# 0  0.975572  0.915725  0.916385  0.923464  0.965646
+# 1  0.930298  0.165725  0.169626  0.217494  0.789177
+# 인덱스 0은 검정통계량, 1번째는 pvalue를 나타내는데 4변수들의 pvalue 모두 0.05보다 크므로 4변수 모두 정규성을 만족한다고 볼 수 있다.
+
+# 위 결과를 종합적으로 보았을 때 실업률을 제외하고 3가지 변수들에 대해 ANOVA분산분석을 실시하겠다.
+
+from scipy import stats
+# 등분산 검정
+print(stats.bartlett(crime_analysis['CPI'],crime_analysis['실업률'], crime_analysis['스트레스 인지율'], crime_analysis['인구수']),stats.fligner(crime_analysis['CPI'],crime_analysis['실업률'], crime_analysis['스트레스 인지율'], crime_analysis['인구수']) ,stats.levene(crime_analysis['CPI'],crime_analysis['실업률'], crime_analysis['스트레스 인지율'], crime_analysis['인구수']), sep="\n")
+# BartlettResult(statistic=901.4731487208453, pvalue=4.241215932978223e-195)
+# FlignerResult(statistic=39.57852798144273, pvalue=1.3088388445622003e-08)
+# LeveneResult(statistic=26.629502809814117, pvalue=7.79363288050101e-11)
 
 
+
+"""
 # 다중선형회귀분석 모델 생성 하기위해 독립,종속 변수 선택 및 모델 생성
 from sklearn.linear_model import LinearRegression
 
@@ -139,12 +167,13 @@ print(Y)
 model = LinearRegression()
 model.fit(X,Y)
 
-"""
 # 다중회귀분석이기 때문에 변수들 간 다중공선성 확인
 X=crime_analysis[['CPI','실업률','스트레스 인지율', '인구수']]
 Y=crime_analysis['범죄건수']
 
 print(stats.f_oneway(X))
+"""
+
 """
 # 예측 모델 생성
 y_pred = model.predict(X)
@@ -165,3 +194,4 @@ import seaborn as sns
 ax1 = sns.displot(Y, label='Y실제 값', kde=True)
 ax2 = sns.displot(y_pred, label='Y예측 값', kde=True)
 plt.show()
+"""
